33464f92878baf6a402021b24314b6a810a13390
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2025-12-17 16:36:56 -0300

    feat: order routes

--- auth_routes.py
+++ auth_routes.py
@@ -2,10 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import get_session
 from main import bcrypt_context
-from schemas import UsuarioSchema
+from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
+
+def criar_token(id_usuario):
+    token = f'fnskhfsh02833r713{id_usuario}'
+    return token
 
 @auth_router.get('/')
 async def home():
@@ -30,4 +34,15 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
         session.add(novo_usuario)
         session.commit()
         return {'mensagem':f'usuário cadastrado com sucesso { usuario_schema.email }'}
-    
+
+@auth_router.post('/login')
+async def login(login_schema: LoginSchema,  session: Session =  Depends(get_session)):
+    usuario = session.query(Usuario).filter(Usuario.email==login_schema.email).first()
+    if not usuario:
+        raise HTTPException(status_code=400, detail='Usuário não encontrado')
+    else:
+        acess_token = criar_token(usuario.id)
+        return {
+                'acess_token':acess_token,
+                'token_type':'Bearer'
+                }
--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import get_session
+from schemas import PedidoSchema
+from models import Pedidos
 
 order_router = APIRouter(prefix='/order', tags=['order'])
 
@@ -10,3 +14,10 @@ async def pedidos():
     Docstring for pedidos
     '''
     return {'voce acessou a rota de pedidos'}
+
+@order_router.post('/pedido')
+async def criar_pedido(pedido_schema: PedidoSchema, session: Session =  Depends(get_session)):
+    novo_pedido = Pedidos(usuario=pedido_schema.id_usuario)
+    session.add(novo_pedido)
+    session.commit()
+    return {'mensagem':f'Pedido criado com sucesso. ID do pedido: {novo_pedido.id}'}
